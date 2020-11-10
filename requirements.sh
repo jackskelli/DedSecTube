b2c084d3992326d3442b4ff0311230534b074cb7
JackSkiller <56979828+jackskelli@users.noreply.github.com>
2020-11-10 16:49:36 +0100

    Update requirements.sh
    Addet ffmpeg

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install -y python-pip
+sudo apt-get install -y python-pip -y
 sudo pip install youtube_dl
 sudo pip install --upgrade youtube_dl
-sudo apt-get install -y libav-tools
+sudo apt-get install -y libav-tools -y
+sudo apt-get install ffmpeg -y
